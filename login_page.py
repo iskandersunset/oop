f95bc47b2d0a942e7980acb97c990e5f546fcc20
Александр Халезин <iskandersunset@gmail.com>
2022-10-19 16:35:26 +0500

    релиз _1

--- login_page.py
+++ login_page.py
@@ -6,24 +6,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Login_page():
+class Login_page:
 
     def __init__(self, driver):
         self.driver = driver
 
     def authorization(self, login_name, login_password):
-
         # 1. Авторизоваться на сайте
-        user_name = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, "user-name")))
+        user_name = WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.ID, "user-name")))
         user_name.send_keys(login_name)
         print("Ввели логин")
-        time.sleep(1)
 
-        user_pass = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, "password")))
+        user_pass = WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.ID, "password")))
         user_pass.send_keys(login_password)
         print("Ввели пароль")
-        time.sleep(1)
 
         user_pass.send_keys(Keys.RETURN)
         print('Нажали Еnter\n', '--' * 20)
-        time.sleep(1)
+
+
