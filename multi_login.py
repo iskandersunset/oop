2e257713a9880e6e190d519f423ccfd890439df8
Александр Халезин <iskandersunset@gmail.com>
2022-10-19 16:36:04 +0500

    Работает пока без проверки ошибки ввода пароля или логина

---
+++ multi_login.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from login_page import Login_page
+from test_first import Test_login
+
+users_login = ('standard_user', 'problem_user', 'performance_glitch_user')
+users_password = 'secret_sauce'
+
+s = Service('C:/_teach/resource/chromedriver.exe')  # Путь на работе
+driver = webdriver.Chrome(service=s)
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+
+for i in range(len(users_login)):
+    print(users_login[i])
+    login = Login_page(driver)
+    login.authorization(users_login[i], users_password)
+    test = Test_login()
+    test.test_login(driver)
+    driver.get(base_url)
+    time.sleep(5)
+
